a165d96a3f0f6f3d191e4eed4911c697556b43d0
Mark Murabayashi <mark.murabayashi@gmail.com>
2019-06-23 08:46:43 -0500

    fix output to fit on phone

--- store_pitcher_true_skill.php
+++ store_pitcher_true_skill.php
@@ -30,7 +30,7 @@ foreach ($KpercentMinusXwoba as $key => $player) {
     $rank = $player['rank'] = $key + 1;
 
     $player['rank_formatted'] = str_pad($rank, 3);
-    $player['rank_last_30_formatted'] = str_pad($player['rank_k_minus_adj_xwoba_last_30'], 3);
+    $player['rank_last_30_formatted'] = str_pad($player['rank_k_minus_adj_xwoba_last_30'] ?? '', 3);
     $player['name_formatted'] = str_pad(substr($player['name'], 0, 18), 18);
     $player['k_percentage_formatted'] = str_pad(number_format($player['k_percentage'], 1), 4, ' ', STR_PAD_LEFT);
     $player['k_percentage_plus_formatted'] = str_pad($player['k_percentage_plus'], 3, ' ', STR_PAD_LEFT);
@@ -41,7 +41,7 @@ foreach ($KpercentMinusXwoba as $key => $player) {
     $KpercentMinusXwoba[$key]['rank'] = $rank;
     $val = $player['val_formatted'] = ((string) (number_format($player['value'] * 100, 1)) . '%');
 
-    echo "{$player['rank_formatted']} | {$player['rank_last_30_formatted']} | {$player['name_formatted']} | {$player['k_percentage_plus_formatted']} K%+ | {$player['velo_formatted']}\n";
+    echo "{$player['rank_formatted']} | {$player['rank_last_30_formatted']} | {$player['name_formatted']} | {$player['k_percentage_plus_formatted']} K+ | {$player['velo_formatted']}\n";
 
     foreach ($custom_lists as $list) {
         if (in_array($player['name'], $players_of_interest[$list])) {
@@ -53,7 +53,7 @@ foreach ($KpercentMinusXwoba as $key => $player) {
 foreach ($custom_lists as $list) {
     echo "\n{$list}\n";
     foreach ($custom_players[$list] as $pitcher) {
-        echo "{$pitcher['rank_formatted']} | {$pitcher['rank_last_30_formatted']} | {$pitcher['name_formatted']} | {$pitcher['k_percentage_plus_formatted']} K%+ | {$pitcher['velo_formatted']}\n";
+        echo "{$pitcher['rank_formatted']} | {$pitcher['rank_last_30_formatted']} | {$pitcher['name_formatted']} | {$pitcher['k_percentage_plus_formatted']} K+ | {$pitcher['velo_formatted']}\n";
     }
 }
 
