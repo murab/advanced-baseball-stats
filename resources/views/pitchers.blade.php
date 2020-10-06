b19c939710050c06da4cddc161fcc072e6cfd54a
Mark Murabayashi <mark.murabayashi@gmail.com>
2020-10-06 12:11:22 -0500

    resize columns

--- resources/views/pitchers.blade.php
+++ resources/views/pitchers.blade.php
@@ -90,7 +90,10 @@
     <script type="text/javascript">
         $(document).ready(function() {
             var t = $('#pitchers').DataTable({
-                paging: false
+                paging: false,
+                columnDefs: [
+                    { "width": "5.5%", "targets": [0,2,3,4,5,6,7,8,9,10,11,12,13,14,15] }
+                ]
             });
 
             // manage index column
