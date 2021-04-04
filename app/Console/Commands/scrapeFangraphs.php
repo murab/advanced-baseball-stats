8b3068ae00102aea02078ea8ee610a126097f851
Mark Murabayashi <mark.murabayashi@gmail.com>
2021-04-04 07:11:04 -0500

    move csw, pa, and velo to fangraphs

--- app/Console/Commands/scrapeFangraphs.php
+++ app/Console/Commands/scrapeFangraphs.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Str;
 
 class scrapeFangraphs extends Command
 {
-    const RAWpitcherDataSource = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48&season=2019&month=0&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
-    const RAWpitcherDataSourceLast30Days = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48&season=2019&month=3&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
-    const RAWpitcherDataSource2ndHalf = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48&season=2019&month=31&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
-    const RAWpitcherDataSource1stHalf = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48&season=2019&month=30&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
+    const RAWpitcherDataSource = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48,332,331,14,139,140,144&season=2019&month=0&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
+    const RAWpitcherDataSourceLast30Days = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48,332,331,14,139,140,144&season=2019&month=3&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
+    const RAWpitcherDataSource2ndHalf = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48,332,331,14,139,140,144&season=2019&month=31&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
+    const RAWpitcherDataSource1stHalf = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=pit&lg=all&qual=0&type=c,36,37,38,40,120,121,217,113,42,43,117,118,6,45,62,122,3,7,8,24,13,310,76,48,332,331,14,139,140,144&season=2019&month=30&season1=2019&ind=0&team=0&rost=0&age=0&filter=&players=0&startdate=&enddate=&page=1_1500';
     const RAWleagueBattersDataSource = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=bat&lg=all&qual=0&type=c,6,39&season=2019&month=0&season1=2019&ind=0&team=0,ss&rost=0&age=0&filter=&players=0&startdate=2019-01-01&enddate=2019-12-31';
     const RAWleaguePitchersDataSource = 'https://www.fangraphs.com/leaders.aspx?pos=all&stats=sta&lg=all&qual=0&type=c,76,113,217,6,42,48&season=2019&month=0&season1=2019&ind=0&team=0,ss&rost=0&age=0&filter=&players=0&startdate=2019-01-01&enddate=2019-12-31';
 
@@ -124,6 +124,10 @@ class scrapeFangraphs extends Command
             $stats->k = $player['k'];
             $stats->k_per_game = $player['k_per_game'];
             $stats->ip = $player['ip'];
+            $stats->pa = $player['pa'];
+            //$stats->xwoba = $player['xera'];
+            $stats->velo = isset($player[$lowername]) ? $player[$lowername]['velo'] : 0;
+            $stats->csw = isset($player[$lowername]) ? $player[$lowername]['csw'] : 0;
 
             //$stats->secondhalf_velo = $data_2nd[$lowername]['velo'] ?? null;
             $stats->secondhalf_k_percentage = $data_2nd[$lowername]['k_percentage'] ?? null;
@@ -136,6 +140,10 @@ class scrapeFangraphs extends Command
             $stats->secondhalf_k = $data_2nd[$lowername]['k'] ?? null;
             $stats->secondhalf_k_per_game = $data_2nd[$lowername]['k_per_game'] ?? null;
             $stats->secondhalf_ip = $data_2nd[$lowername]['ip'] ?? null;
+            $stats->secondhalf_pa = $data_2nd[$lowername]['pa'] ?? null;
+            //$stats->secondhalf_xwoba = $data_2nd[$lowername]['xera'] ?? null;
+            $stats->secondhalf_velo = isset($velo_2nd[$lowername]) ? $data_2nd[$lowername]['velo'] : 0;
+            $stats->secondhalf_csw = isset($data_2nd[$lowername]) ? $data_2nd[$lowername]['csw'] : 0;
 
             $stats->save();
         }
@@ -251,43 +259,64 @@ class scrapeFangraphs extends Command
         $player_data = [];
         foreach ($stats as $stat) {
 
-            if ($i%27 == 1) {
+            if ($i%33 == 1) {
                 $player_data = [];
                 // Name
                 $player_data['name'] = hQuery::fromHTML($stat->innerHTML)->find('a')->innerHTML;
                 $player_data['name'] = preg_replace("/[^A-Za-z0-9\- ]/", '', $player_data['name']);
-            } elseif ($i%27 == 7) {
+            } elseif ($i%33 == 7) {
                 // K%
                 $player_data['k_percentage'] = floatval($stat->innerHTML);
-            } elseif ($i%27 == 8) {
+            } elseif ($i%33 == 8) {
                 // BB%
                 $player_data['bb_percentage'] = floatval($stat->innerHTML);
-            } elseif ($i%27 == 9) {
+            } elseif ($i%33 == 9) {
                 // K-BB%
                 $player_data['kbb_percentage'] = floatval($stat->innerHTML);
-            } elseif ($i%27 == 10) {
+            } elseif ($i%33 == 10) {
                 // SwStr%
                 $player_data['swstr_percentage'] = floatval($stat->innerHTML);
-            } elseif ($i%27 == 19) {
+            } elseif ($i%33 == 19) {
                 // Age
                 $player_data['age'] = (int) $stat->innerHTML;
-            } elseif ($i%27 == 20) {
+            } elseif ($i%33 == 20) {
                 // Games
                 $player_data['g'] = (int) $stat->innerHTML;
-            } elseif ($i%27 == 21) {
+            } elseif ($i%33 == 21) {
                 // Games
                 $player_data['gs'] = (int) $stat->innerHTML;
-            } elseif ($i%27 == 22) {
+            } elseif ($i%33 == 22) {
                 // Games
                 $player_data['k'] = (int) $stat->innerHTML;
-            } elseif ($i%27 == 23) {
+            } elseif ($i%33 == 23) {
                 $player_data['ip'] = $stat->innerHTML;
-            } elseif ($i%27 == 24) {
+            } elseif ($i%33 == 24) {
                 $player_data['k_percentage_plus'] = $stat->innerHTML;
-            } elseif ($i%27 == 25) {
+            } elseif ($i%33 == 25) {
                 $player_data['velo'] = (float) $stat->innerHTML;
-            } elseif ($i%27 == 26) {
-                $player_data['gb_percentage'] = (float) $stat->innerHTML;
+            } elseif ($i%33 == 26) {
+                $player_data['gb_percentage'] = (float)$stat->innerHTML;
+            } elseif ($i%33 == 27) {
+                $player_data['xera'] = (float)$stat->innerHTML;
+            } elseif ($i%33 == 28) {
+                $player_data['csw'] = (float)$stat->innerHTML;
+            } elseif ($i%33 == 29) {
+                $player_data['pa'] = (int)$stat->innerHTML;
+            } elseif ($i%33 == 30) {
+                if (!empty(trim($stat->innerHTML))) {
+                    $velo = (float)$stat->innerHTML;
+                    $player_data['velo'] = max($velo, ($player_data['velo'] ?? 0));
+                }
+            } elseif ($i%33 == 31) {
+                if (!empty(trim($stat->innerHTML))) {
+                    $velo = (float)$stat->innerHTML;
+                    $player_data['velo'] = max($velo, ($player_data['velo'] ?? 0));
+                }
+            } elseif ($i%33 == 32) {
+                if (!empty(trim($stat->innerHTML))) {
+                    $velo = (float)$stat->innerHTML;
+                    $player_data['velo'] = max($velo, ($player_data['velo'] ?? 0));
+                }
                 $data[strtolower($player_data['name'])] = [
                     'name' => $player_data['name'],
                     'k_percentage' => $player_data['k_percentage'],
@@ -302,7 +331,10 @@ class scrapeFangraphs extends Command
                     'k_per_game' => $player_data['k'] / $player_data['g'],
                     'gs' => $player_data['gs'],
                     'ip' => $player_data['ip'],
-                    'velo' => $player_data['velo']
+                    'csw' => $player_data['csw'],
+                    'pa' => $player_data['pa'],
+                    //'xwoba' => $player_data['xera'],
+                    'velo' => $player_data['velo'],
                 ];
             }
             $i++;
--- app/Console/Commands/scrapeSavant.php
+++ app/Console/Commands/scrapeSavant.php
@@ -12,17 +12,9 @@ class scrapeSavant extends Command
 {
     const RAWpitchersXwobaURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0&chk_stats_pa=on&chk_stats_xwoba=on#results';
     const RAWpitchersXwoba2ndHalfURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=2019-07-09&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=xwoba&player_event_sort=h_launch_speed&sort_order=asc&min_pas=0&chk_stats_pa=on&chk_stats_xwoba=on#results';
-    const RAWpitchersVeloURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=FF%7CFT%7CSI%7C&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=velocity&player_event_sort=h_launch_speed&sort_order=desc&min_pas=0&chk_stats_velocity=on#results';
-    const RAWpitchersVelo2ndHalfURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=FF%7CFT%7CSI%7C&hfAB=&hfBBT=&hfPR=&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=2019-07-09&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=velocity&player_event_sort=h_launch_speed&sort_order=desc&min_pas=0&chk_stats_velocity=on#results';
-    const RAWpitchersCswURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=called%5C.%5C.strike%7Cfoul%5C.%5C.tip%7Cswinging%5C.%5C.pitchout%7Cswinging%5C.%5C.strike%7Cswinging%5C.%5C.strike%5C.%5C.blocked%7C&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitch_percent&player_event_sort=api_h_launch_speed&sort_order=desc&min_pas=0&chk_stats_pa=on&chk_stats_xwoba=on#results';
-    const RAWpitchersCsw2ndHalfURL = 'https://baseballsavant.mlb.com/statcast_search?hfPT=&hfAB=&hfBBT=&hfPR=called%5C.%5C.strike%7Cfoul%5C.%5C.tip%7Cswinging%5C.%5C.pitchout%7Cswinging%5C.%5C.strike%7Cswinging%5C.%5C.strike%5C.%5C.blocked%7C&hfZ=&stadium=&hfBBL=&hfNewZones=&hfGT=R%7C&hfC=&hfSea=2019%7C&hfSit=&player_type=pitcher&hfOuts=&opponent=&pitcher_throws=&batter_stands=&hfSA=&game_date_gt=2019-07-09&game_date_lt=&hfInfield=&team=&position=&hfOutfield=&hfRO=&home_road=&hfFlag=&hfPull=&metric_1=&hfInn=&min_pitches=0&min_results=0&group_by=name&sort_col=pitch_percent&player_event_sort=api_h_launch_speed&sort_order=desc&min_pas=0&chk_stats_pa=on&chk_stats_xwoba=on#results';
 
     private $pitchersXwobaURL;
     private $pitchersXwoba2ndHalfURL;
-    private $pitchersVeloURL;
-    private $pitchersVelo2ndHalfURL;
-    private $pitchersCswURL;
-    private $pitchersCsw2ndHalfURL;
 
     /**
      * The name and signature of the console command.
@@ -68,10 +60,6 @@ class scrapeSavant extends Command
 
         $data = $this->getpitchersXwobaData();
         $data_2nd = $this->getPitchersXwobaData2ndHalf();
-        $velo = $this->getpitchersVeloData();
-        $velo_2nd = $this->getPitchersVeloData2ndHalf();
-        $csw = $this->getPitchersCswData();
-        $csw_2nd = $this->getPitchersCswData2ndHalf();
 
         foreach ($data as $player) {
             $Player = Player::firstOrCreate([
@@ -86,14 +74,8 @@ class scrapeSavant extends Command
                 'year' => $year,
             ]);
 
-            $stats->pa = $player['pa'];
-            $stats->secondhalf_pa = $data_2nd[$lowername]['pa'] ?? null;
             $stats->xwoba = $player['xwoba'];
             $stats->secondhalf_xwoba = $data_2nd[$lowername]['xwoba'] ?? null;
-            $stats->velo = isset($velo[$lowername]) ? $velo[$lowername]['velo'] : 0;
-            $stats->secondhalf_velo = isset($velo_2nd[$lowername]) ? $velo_2nd[$lowername]['velo'] : 0;
-            $stats->csw = isset($csw[$lowername]) ? $csw[$lowername]['csw'] : 0;
-            $stats->secondhalf_csw = isset($csw_2nd[$lowername]) ? $csw_2nd[$lowername]['csw'] : 0;
             $stats->save();
         }
 
@@ -104,16 +86,12 @@ class scrapeSavant extends Command
     {
         $this->pitchersXwobaURL = str_replace('2019',$year,self::RAWpitchersXwobaURL);
         $this->pitchersXwoba2ndHalfURL = str_replace('2019', $year, self::RAWpitchersXwoba2ndHalfURL);
-        $this->pitchersVeloURL = str_replace('2019',$year,self::RAWpitchersVeloURL);
-        $this->pitchersVelo2ndHalfURL = str_replace('2019',$year,self::RAWpitchersVelo2ndHalfURL);
-        $this->pitchersCswURL = str_replace('2019',$year,self::RAWpitchersCswURL);
-        $this->pitchersCsw2ndHalfURL = str_replace('2019',$year,self::RAWpitchersCsw2ndHalfURL);
     }
 
     private function parseXwobaData($players) {
         $data = [];
 
-        if (is_array($players)) foreach ($players as $player) {
+        if (is_iterable($players)) foreach ($players as $player) {
 
             $vals = $player->find('td');
 
@@ -148,79 +126,13 @@ class scrapeSavant extends Command
         return $data;
     }
 
-    private function parseVeloData($players) {
-        $data = [];
-
-        if (is_array($players)) foreach ($players as $player) {
-
-            $vals = $player->find('td');
-
-            $i = 0;
-            $player_data = [];
-            foreach ($vals as $val) {
-
-                if ($i == 2) {
-                    // Name
-                    $name = explode(', ', $val->innerHTML);
-                    $player_data['name'] = trim($name[1]) . ' ' . trim($name[0]);
-                    $player_data['name'] = trim(preg_replace("/[^A-Za-z0-9\- ]/", '', $player_data['name']));
-                } elseif ($i == 6) {
-                    // Velo
-                    $player_data['velo'] = (str_replace(['<span>','</span>'],'',$val->innerHTML));
-                }
-
-                $i++;
-            }
-            if (!empty($player_data['velo'])) {
-                $data[strtolower($player_data['name'])] = [
-                    'name' => $player_data['name'],
-                    'velo' => $player_data['velo'],
-                ];
-            }
-        }
-        return $data;
-    }
-
-    private function parseCswData($players) {
-        $data = [];
-
-        if (is_array($players)) foreach ($players as $player) {
-
-            $vals = $player->find('td');
-
-            $i = 0;
-            $player_data = [];
-            foreach ($vals as $val) {
-
-                if ($i == 2) {
-                    // Name
-                    $name = explode(', ', $val->innerHTML);
-                    $player_data['name'] = trim($name[1]) . ' ' . trim($name[0]);
-                    $player_data['name'] = trim(preg_replace("/[^A-Za-z0-9\- ]/", '', $player_data['name']));
-                } elseif ($i == 5) {
-                    // CSW
-                    $player_data['csw'] = (str_replace(['<span>','</span>'],'',$val->innerHTML));
-                }
-
-                $i++;
-            }
-            if (!empty($player_data['csw'])) {
-                $data[strtolower($player_data['name'])] = [
-                    'name' => $player_data['name'],
-                    'csw' => $player_data['csw'],
-                ];
-            }
-        }
-        return $data;
-    }
-
     public function getpitchersXwobaData()
     {
         hQuery::$cache_expires = 0;
 
         $doc = hQuery::fromUrl($this->pitchersXwobaURL, [
             'Accept'     => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
+            //'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
         ]);
 
         $players = $doc->find('#search_results tbody tr.search_row');
@@ -243,64 +155,6 @@ class scrapeSavant extends Command
 
         $players = $doc->find('#search_results tbody tr.search_row');
         $data = $this->parseXwobaData($players);
-        return $data;
-    }
-
-    public function getpitchersVeloData()
-    {
-        hQuery::$cache_expires = 0;
-
-        $doc = hQuery::fromUrl($this->pitchersVeloURL, [
-            'Accept'     => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        ]);
-
-        $players = $doc->find('#search_results tbody tr.search_row');
-
-        $data = [];
-        if ($players) {
-            $data = $this->parseVeloData($players);
-        }
-
-        return $data;
-    }
-
-    public function getPitchersVeloData2ndHalf()
-    {
-        hQuery::$cache_expires = 0;
-        $doc = hQuery::fromUrl($this->pitchersVelo2ndHalfURL, [
-            'Accept'     => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        ]);
-
-        $players = $doc->find('#search_results tbody tr.search_row');
-        $data = $this->parseVeloData($players);
-        return $data;
-    }
-
-    public function getPitchersCswData()
-    {
-        hQuery::$cache_expires = 0;
-        $doc = hQuery::fromUrl($this->pitchersCswURL, [
-            'Accept'     => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        ]);
-
-        $players = $doc->find('#search_results tbody tr.search_row');
-        $data = $this->parseCswData($players);
-        return $data;
-    }
-
-    public function getPitchersCswData2ndHalf()
-    {
-        hQuery::$cache_expires = 0;
-        $doc = hQuery::fromUrl($this->pitchersCsw2ndHalfURL, [
-            'Accept'     => 'text/html,application/xhtml+xml;q=0.9,*/*;q=0.8',
-            'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36',
-        ]);
-
-        $players = $doc->find('#search_results tbody tr.search_row');
-        $data = $this->parseCswData($players);
         return $data;
     }
 }
