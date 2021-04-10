209fd1b22d0e9b7c4377e5456ad02b347100c603
Mark Murabayashi <mark.murabayashi@gmail.com>
2021-04-10 08:38:07 -0500

    fix migration

--- database/migrations/2021_04_10_052822_add_hitter_ranks.php
+++ database/migrations/2021_04_10_052822_add_hitter_ranks.php
@@ -17,8 +17,7 @@ class AddHitterRanks extends Migration
             $table->float('sprint_speed')->nullable();
             $table->float('brls_bbe')->nullable();
             $table->float('rank_avg')->nullable();
-
-            $table->float('secondhalf_k_percentage')->nullable();
+            
             $table->float('secondhalf_brls_bbe')->nullable();
 
             $table->integer('k_percentage_rank')->nullable();
@@ -36,7 +35,7 @@ class AddHitterRanks extends Migration
     public function down()
     {
         Schema::table('hitters', function (Blueprint $table) {
-            $table->dropColumn(['secondhalf_k_percentage', 'secondhalf_brls_bbe', 'rank_avg', 'k_percentage_rank', 'sprint_speed', 'brls_bbe', 'sprint_speed_rank', 'hardhit_rank', 'brls_bbe_rank']);
+            $table->dropColumn(['secondhalf_brls_bbe', 'rank_avg', 'k_percentage_rank', 'sprint_speed', 'brls_bbe', 'sprint_speed_rank', 'hardhit_rank', 'brls_bbe_rank']);
         });
     }
 }
