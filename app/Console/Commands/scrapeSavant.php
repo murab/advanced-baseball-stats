dfcee0a6e0d60e83c6554dd190407e048fea2088
Mark Murabayashi <mark.murabayashi@gmail.com>
2021-06-26 06:38:20 -0500

    fix jc mejia

--- app/Console/Commands/scrapeSavant.php
+++ app/Console/Commands/scrapeSavant.php
@@ -19,6 +19,7 @@ class scrapeSavant extends Command
     const namesSavantToFangraphs = [
         'Cedric Mullins' => 'Cedric Mullins II',
         'Jazz Chisholm Jr' => 'Jazz Chisholm',
+        'JC Mejia' => 'Jean Carlos Mejia',
     ];
 
     private $pitchersXwobaURL;
