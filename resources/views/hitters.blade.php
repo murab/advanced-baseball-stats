07b94831e798cc6ed6ea0c8513059cbe15241b80
Mark Murabayashi <mdm6x7@gmail.com>
2023-04-24 05:24:53 -0500

    remove striping from hitters table

--- resources/views/hitters.blade.php
+++ resources/views/hitters.blade.php
@@ -289,9 +289,9 @@
                     $('.numHitters').html(rank-1);
                 });
 
-                $("tr:visible").each(function (index) {
-                    $(this).css("background-color", !!(index & 1)? "rgba(0,0,0,.05)" : "rgba(0,0,0,0)");
-                });
+                // $("tr:visible").each(function (index) {
+                //     $(this).css("background-color", !!(index & 1)? "rgba(0,0,0,.05)" : "rgba(0,0,0,0)");
+                // });
             }
 
             doMins();
