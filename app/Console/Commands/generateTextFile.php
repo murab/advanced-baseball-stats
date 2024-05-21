d97bfff88be2162e5d1aa2ddeadc9ad85c9515fa
Mark Murabayashi <mdm6x7@gmail.com>
2024-05-21 05:55:04 -0500

    match min ip in text file to website

--- app/Console/Commands/generateTextFile.php
+++ app/Console/Commands/generateTextFile.php
@@ -57,8 +57,11 @@ class generateTextFile extends Command
         $custom_lists = array_keys($players_of_interest);
         $custom_players = [];
 
-        $starters = Stat::where(['year' => $year, 'position' => 'SP', ['tru', '<>', null]])->with('player')->orderBy('tru_rank', 'asc')->get()->toArray();
-        $relievers = Stat::where(['year' => $year, 'position' => 'RP', ['tru', '<>', null]])->with('player')->orderBy('tru_rank', 'asc')->get()->toArray();
+        $sp_ip_min = Stat::calculateMinInningsPitched($year, 'SP');
+        $rp_ip_min = Stat::calculateMinInningsPitched($year, 'RP');
+
+        $starters = Stat::where(['year' => $year, ['ip', '>=', $sp_ip_min], 'position' => 'SP', ['tru', '<>', null]])->with('player')->orderBy('tru_rank', 'asc')->get()->toArray();
+        $relievers = Stat::where(['year' => $year, ['ip', '>=', $rp_ip_min], 'position' => 'RP', ['tru', '<>', null]])->with('player')->orderBy('tru_rank', 'asc')->get()->toArray();
 
         $league = Stat::leagueAverageStats($year);
 
